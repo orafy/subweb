7ae006dc94615ed16f5adb5b0af7adc66f19be85
orafy <myuserc1@outlook.com>
2024-07-23 01:34:37 +0900

    Add files via upload

---
+++ conf/config.js
@@ -0,0 +1,25 @@
+window.config = {
+  // 网站标题
+  siteName: 'Subconverter Web',
+  // API 地址
+  apiUrl: 'https://a.y1z.org',
+  // 短域名服务地址
+  shortUrl: 'https://suo.yt',
+  // 默认远程配置
+  remoteConfigName: '默认配置',
+  // 默认远程配置地址
+  remoteConfigUrl: 'https://jsd.onmicrosoft.cn/gh/ACL4SSR/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini',
+  // 首页菜单
+  menuItem: [
+    {
+      title: '首页',
+      link: '/',
+      target: '',
+    },
+    {
+      title: 'GitHub',
+      link: 'https://github.com/orafy/subweb',
+      target: '_blank',
+    },
+  ],
+};
