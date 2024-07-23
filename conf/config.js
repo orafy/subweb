d30e5b8ac0ed9735694af60f89a6ffc3d5c5c2de
orafy <myuserc1@outlook.com>
2024-07-23 23:16:27 +0900

    Add files via upload

--- conf/config.js
+++ conf/config.js
@@ -8,7 +8,7 @@ window.config = {
   // 默认远程配置
   remoteConfigName: '默认配置',
   // 默认远程配置地址
-  remoteConfigUrl: 'https://jsd.onmicrosoft.cn/gh/ACL4SSR/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini',
+  remoteConfigUrl: 'https://cdn.jsdmirror.com/gh/ACL4SSR/ACL4SSR@master/Clash/config/ACL4SSR_Online_Mini_NoAuto.ini',
   // 首页菜单
   menuItem: [
     {
@@ -17,8 +17,8 @@ window.config = {
       target: '',
     },
     {
-      title: 'GitHub',
-      link: 'https://github.com/orafy/subweb',
+      title: 'ip',
+      link: 'http://34.36.47.246/api/ip',
       target: '_blank',
     },
   ],
